53f80e841cd8bd903db1befc62dae98a343eabef
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-04-11 01:18:40 +0530

    Time: 0 ms (100%), Space: 42.5 MB (56.28%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,8 +3,6 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if((i==0||nums[i]>nums[i-1])&&(i==nums.length-1||nums[i]>nums[i+1]))
             return i;
-            if(i!=nums.length-1&&nums[i]>=nums[i+1])
-            i++;
         }
         return 0;
     }
