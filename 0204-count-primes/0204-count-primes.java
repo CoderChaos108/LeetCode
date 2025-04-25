9b9bfc8e664e0f9a927931938675ca6fcdeb23dc
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-04-25 18:57:08 +0530

    Time: 126 ms (18.36%), Space: 48.8 MB (97.45%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,19 +1,19 @@
 class Solution {
     public int countPrimes(int n) {
-         char[] IsPrime=new char[n];
+         boolean[] IsPrime=new boolean[n];
         int c=0;
        for(int i=2;i<n;i++){
-            if(IsPrime[i]=='n')
+            if(IsPrime[i]==true)
             continue;
            if(i==2||i==3||i==5||i==7)
-           IsPrime[i]='y';
-           if((IsPrime[i]!='n'&&i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0)||IsPrime[i]=='y'){
-            IsPrime[i]='y';
+           IsPrime[i]=false;
+           if((IsPrime[i]!=true&&i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0)||IsPrime[i]==false){
+            IsPrime[i]=false;
             for(int j=2;i*j<n;j++){
-                IsPrime[i*j]='n';
+                IsPrime[i*j]=true;;
             }
            }
-           if(IsPrime[i]=='y')
+           if(IsPrime[i]==false)
            c++;
        }
         return c;
