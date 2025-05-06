bea0f9d9a121be2cd41d59115c60ff55b22ac1b1
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-06 17:30:34 +0530

    Time: 3 ms (85.02%), Space: 45.3 MB (25.39%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,18 +1,32 @@
 class Solution {
     public String reverseVowels(String s) {
-        char t='0';
-        ArrayList <Integer> a=new ArrayList<Integer>();
-        StringBuilder sb=new StringBuilder(s);
-        for(int i=0;i<s.length();i++){
-            t=Character.toLowerCase(s.charAt(i));
-            if(t=='a'||t=='e'||t=='i'||t=='o'||t=='u')
-            a.add(i);
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+        
+        while (start < end) {
+            // Move start pointer until it points to a vowel
+            while (start < end && vowels.indexOf(word[start]) == -1) {
+                start++;
+            }
+            
+            // Move end pointer until it points to a vowel
+            while (start < end && vowels.indexOf(word[end]) == -1) {
+                end--;
+            }
+            
+            // Swap the vowels
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+            
+            // Move the pointers towards each other
+            start++;
+            end--;
         }
-        for(int i=0;i<=a.size()/2-1;i++){
-            t=sb.charAt(a.get(a.size()-i-1));
-            sb.setCharAt(a.get(a.size()-i-1),sb.charAt(a.get(i)));
-            sb.setCharAt(a.get(i),t);
-        }
-        return sb.toString();
+        
+        String answer = new String(word);
+        return answer;
     }
 }
