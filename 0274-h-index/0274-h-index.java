118d3f4ce8a33e4509a704f0f0871d2362dc6ee6
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-11 03:44:22 +0530

    Time: 0 ms (100%), Space: 41.5 MB (81.25%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,9 +1,21 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        for(int i=0;i<citations.length;i++){
-            if(citations.length-i<=citations[i])
-            return citations.length-i;
+        int l=0;
+        for(int i:citations){
+            if(l<i)
+            l=i;
+        }
+        int[] f=new int[l+1];
+        for(int i:citations){
+            f[i]++;
+        }
+        l=0;
+        for(int i=0;i<f.length;i++){
+            for(int j=0;j<f[i];j++){
+                if(citations.length-l<=i)
+                return citations.length-l;
+                l++;
+            }
         }
         return 0;
     }
