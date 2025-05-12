1b88538937c1723ea5df01d95658338753ce9b70
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-12 23:54:46 +0530

    Time: 2 ms (70.83%), Space: 42 MB (12.76%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,20 +1,45 @@
 class Solution {
     public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        if(citations.length==1){
+            if(citations[0]==0)
+            return 0;
+            else
+            return 1;
+        }
+        if(citations.length==2){
+            if(citations[0]==0&&citations[1]!=0)
+            return 1;
+            else if(citations[0]==0&&citations[1]==0)
+            return 0;
+            else if(citations[0]==1&&citations[1]==1)
+            return 1;
+            else if(citations[0]==1&&citations[1]==2)
+            return 1;
+            else 
+            return 2;
+        }
+        int r=citations.length-1;
         int l=0;
-        for(int i:citations){
-            if(l<i)
-            l=i;
-        }
-        int[] f=new int[l+1];
-        for(int i:citations){
-            f[i]++;
-        }
-        l=0;
-        for(int i=0;i<f.length;i++){
-            for(int j=0;j<f[i];j++){
-                if(citations.length-l<=i)
+        int m=(r+l)/2;
+        while(l>-1){
+            if(r-l<2){
+                if(citations.length-l<=citations[l])
                 return citations.length-l;
-                l++;
+                else if(citations.length-r<=citations[r])
+                return citations.length-r;
+                else
+                return 0;
+            }
+            m=(r+l)/2;
+            if(citations.length-m<=citations[m]){
+                if(m!=0&&citations.length-m+1<=citations[m-1])
+                r=m;
+                else
+                return citations.length-m;
+            }
+            else{
+                l=m;
             }
         }
         return 0;
