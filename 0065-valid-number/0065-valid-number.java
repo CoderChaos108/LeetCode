d8f284c94114e602896c3f100e02f0d2ebc11c9b
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-16 04:09:01 +0530

    Time: 9 ms (12.01%), Space: 44 MB (12.73%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean isNumber(String s) {
-        int l=0,r=s.length()-1;
-        while(l<=r){
-            char cl=s.charAt(l),cr=s.charAt(r);
-            if(Character.isLetter(cl)){
-                if(cl!='e'&&cl!='E')
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetter(s.charAt(i))){
+                if(s.charAt(i)!='e'&&s.charAt(i)!='E')
                 return false;
             }
-            if(l!=r&&Character.isLetter(cr)){
-                if(cr!='e'&&cr!='E')
-                return false;
-            }
-            l++;
-            r--;
         }
         try{
         Double d=Double.valueOf(s);
