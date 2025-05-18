c3a07b65a0803ada3e0721d845c767a2e6b782e1
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-19 05:09:09 +0530

    Time: 1 ms (84.99%), Space: 42.2 MB (25.09%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,31 +1,25 @@
 class Solution {
-    static int C(int u,int d){
-        if(d==0||u==d)
-        return 1;
-        if(d>=u-d)
-        d=u-d;
-        long p=1;
-        for(int i=0;i<d;i++){
-            p=p*u;
-            u--;
-        }
-        long q=1;
-        while(d!=1){
-            q=d*q;
-            d--;
-        }
-        return (int)(p/q);
-
-    }
     public List<List<Integer>> generate(int n) {
         ArrayList<List<Integer>> ans=new ArrayList<List<Integer>>();
-        for(int i=0;i<n;i++){
-            ArrayList<Integer> list=new ArrayList<Integer>();
-            for(int j=0;j<=i;j++){
-                list.add(C(i,j));
+            for(int i=0;i<n;i++){
+                ArrayList<Integer> list=new ArrayList<Integer>();
+                if(i==0)
+                list.add(1);
+                else if(i==1){
+                list.add(1);
+                list.add(1);
+                }
+                else{
+                ArrayList<Integer> temp = new ArrayList<>(ans.get(i-1));
+                for(int j=0;j<=i;j++){
+                    if(j==0||j==i)
+                    list.add(1);
+                    else
+                    list.add(temp.get(j)+temp.get(j-1));
             }
+                }
             ans.add(list);
-        }
-        return ans;
+            }
+            return ans;
     }
 }
