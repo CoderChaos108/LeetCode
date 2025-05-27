42cd0f1e5b4ee90344d3bc20804291fd16228139
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-27 14:45:23 +0530

    Time: 2 ms (48.08%), Space: 41.3 MB (52.87%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,13 +1,13 @@
 class Solution {
     public static String getPermutation(int n, int k) {
         int[] f=new int[n+1];
+        StringBuilder s=new StringBuilder();
         int m=1;
         f[0]=1;
         for(int i=1;i<=n;i++){
         m=m*i;
         f[i]=m;
         }
-        String s="";
         int sum=0;
         HashSet<Integer> set=new HashSet<>();
         for(int j=0;j<n;j++){
@@ -17,23 +17,23 @@ class Solution {
                 continue;
                 sum=sum+f[n-1-s.length()];
                 if(sum==k){
-                    s=s+String.valueOf(i);
+                    s.append(String.valueOf(i));
                     set.add(i);
                     for(int t=n;t>=1;t--){
                         if(!set.contains(t)){
-                            s=s+String.valueOf(t);
+                            s.append(String.valueOf(t));
                         }
                     }
-                    return s;
+                    return s.toString();
                 }
                 if(sum>k){
-                    s=s+String.valueOf(i);
+                    s.append(String.valueOf(i));
                     set.add(i);
                     k=k-sum+f[n-s.length()];
                     break;
                 }
             }
         }
-        return s;
+        return s.toString();
     }
 }
