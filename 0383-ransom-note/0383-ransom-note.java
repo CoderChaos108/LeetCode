6666978def3786865a8e0ade4e2c237f00bb2156
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-28 22:40:00 +0530

    Time: 1 ms (99.35%), Space: 44.5 MB (96.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean canConstruct(String s1, String s2) {
         int[] a=new int[26];
-        int l=s1.length();
-        for(int i=0;i<l;i++){
-            a[s1.charAt(i)-'a']++;
-        }
-        l=s2.length();
+        int l=s2.length();
         for(int i=0;i<l;i++){
             a[s2.charAt(i)-'a']--;
         }
+        l=s1.length();
+        for(int i=0;i<l;i++){
+            a[s1.charAt(i)-'a']++;
+            if(a[s1.charAt(i)-'a']==1)
+            return false;
+        }
+        
         for(int i:a){
             if(i>0)
             return false;
