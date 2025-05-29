0dbd52d80b252fe74e6c1da9519856895f867b44
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-05-29 14:31:35 +0530

    Time: 55 ms (5.04%), Space: 46.9 MB (8.59%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,72 +1,57 @@
-import java.util.*;
 class Solution {
     public static String preprocess(String input) {
-        input=input.replaceAll("\\-\\(", "-1*(");
-        return input;
+        return input.replaceAll("\\-\\(","-1*(");
     }
     public static int parse(String s) {
         StringBuilder sb=new StringBuilder();
-        ArrayList<String> list=new ArrayList<>();
+        List<String> list=new ArrayList<>();
         int l=s.length();
         int i=0;
         while(i<l){
-            if(s.charAt(i)==' '){
+            char c=s.charAt(i);
+            if(c==' '){
                 i++;
                 continue;
             }
-            if(s.charAt(i)=='-'&&(i==0||!Character.isDigit(s.charAt(i-1))&&s.charAt(i-1)!=')')){
-                sb.append('-');
+            if(c=='-'&&(i==0||(!Character.isDigit(s.charAt(i-1))&&s.charAt(i-1)!=')'))){
+                sb.append(c);
                 i++;
-                while(i<l&&Character.isDigit(s.charAt(i))){
-                    sb.append(s.charAt(i));
-                    i++;
-                }
+                while(i<l&&Character.isDigit(s.charAt(i)))
+                    sb.append(s.charAt(i++));
                 list.add(sb.toString());
                 sb.setLength(0);
-            }else if(Character.isDigit(s.charAt(i))){
-                while(i<l&&Character.isDigit(s.charAt(i))){
-                    sb.append(s.charAt(i));
-                    i++;
-                }
+            }else if(Character.isDigit(c)){
+                while(i<l&&Character.isDigit(s.charAt(i)))
+                    sb.append(s.charAt(i++));
                 list.add(sb.toString());
                 sb.setLength(0);
             }else{
-                list.add(s.charAt(i)+"");
+                list.add(String.valueOf(c));
                 i++;
             }
         }
-        String a,b;
         int x=0;
         for(i=0;i<list.size();i++){
-            if(list.get(i).equals("/")){
-                a=list.get(i-1);
-                b=list.get(i+1);
-                x=(Integer.valueOf(a))/(Integer.valueOf(b));
+            String op=list.get(i);
+            if(op.equals("/")||op.equals("*")){
+                int a=Integer.parseInt(list.get(i-1));
+                int b=Integer.parseInt(list.get(i+1));
+                x=op.equals("/")?a/b:a*b;
                 list.set(i-1,String.valueOf(x));
                 list.remove(i+1);
                 list.remove(i);
-                i=i-1;
-            }
-            if(i<list.size()&&list.get(i).equals("*")){
-                a=list.get(i-1);
-                b=list.get(i+1);
-                x=(Integer.valueOf(a))*(Integer.valueOf(b));
-                list.set(i-1,String.valueOf(x));
-                list.remove(i+1);
-                list.remove(i);
-                i=i-1;
+                i--;
             }
         }
         int sign=1;
         x=0;
         for(String t:list){
-            if(t.equals("+")){
+            if(t.equals("+"))
                 sign=1;
-            }else if(t.equals("-")){
+            else if(t.equals("-"))
                 sign=-1;
-            }else{
-                x+=Integer.valueOf(t)*sign;
-            }
+            else
+                x+=Integer.parseInt(t)*sign;
         }
         return x;
     }
@@ -74,10 +59,10 @@ class Solution {
         s=preprocess('('+s+')');
         Stack<Character> st=new Stack<>();
         StringBuilder sb=new StringBuilder();
-        int l=s.length();
         int k=0;
-        for(int i=0;i<l;i++){
-            if(s.charAt(i)==')'){
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c==')'){
                 sb.setLength(0);
                 while(st.peek()!='(')
                     sb.insert(0,st.pop());
@@ -89,7 +74,7 @@ class Solution {
                     st.push(sb.charAt(j));
                 sb.setLength(0);
             }else
-                st.push(s.charAt(i));
+                st.push(c);
         }
         sb.setLength(0);
         while(!st.isEmpty())
