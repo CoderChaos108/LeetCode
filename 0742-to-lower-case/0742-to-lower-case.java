4445f02787dd2f13751256672c4542c00758aade
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-06-11 03:57:34 +0530

    Time: 0 ms (100%), Space: 41.8 MB (29.74%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,12 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
         StringBuilder sb=new StringBuilder();
-       for(int i=0;i<s.length();i++){
-        if(s.charAt(i)>=65&&s.charAt(i)<=90){
-            sb.append((char)(s.charAt(i)+32));
+        int l=s.length();
+        char c;
+       for(int i=0;i<l;i++){
+        c=s.charAt(i);
+        if(c>=65&&c<=90){
+            sb.append((char)(c+32));
         }
         else
-        sb.append(s.charAt(i));
+        sb.append(c);
        }
        return sb.toString();
     }
