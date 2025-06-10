24bbbd6ae5be5972c15717cf2fb6611ffc7a795d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-06-11 05:04:02 +0530

    Time: 67 ms (29.69%), Space: 45.2 MB (95.84%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -4,17 +4,24 @@ class MyCalendar {
      list=new ArrayList<>();   
     }
     public boolean book(int startTime, int endTime) {
+        endTime=endTime-1;
         if(list.size()==0){
-            int a[]={startTime,endTime-1};
+            int a[]={startTime,endTime};
             list.add(a);
             return true;
         }
         for(int[] a:list){
-            if(startTime<=a[1]&&a[0]<endTime)
-            return false;      
+            if(a[0]>=startTime&&a[0]<=endTime)
+            return false;
+            if(a[1]>=startTime&&a[1]<=endTime)
+            return false;
+            if(startTime>=a[0]&&startTime<=a[1])
+            return false;
+            if(endTime>=a[0]&&endTime<=a[1])
+            return false;          
         }
        
-        int[] a={startTime,endTime-1};
+        int[] a={startTime,endTime};
         list.add(a);
         return true;
     }
