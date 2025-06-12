88fe222afc40447e40abd44160341078d9a50d6b
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-06-12 23:00:23 +0530

    Time: 2 ms (95.67%), Space: 42 MB (31.12%) - LeetHub

--- 0822-unique-morse-code-words/0822-unique-morse-code-words.java
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.java
@@ -1,16 +1,12 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
         String[] arr={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        HashMap<Character,String> hm=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            hm.put((char)('a'+i),arr[i]);
-        }
         StringBuilder sb=new StringBuilder();
         HashSet<String> set=new HashSet<>();
         for(String s:words){
             sb.setLength(0);
             for(int i=0;i<s.length();i++){
-                sb.append(hm.get(s.charAt(i)));
+                sb.append(arr[s.charAt(i)-'a']);
             }
             set.add(sb.toString());
         }
