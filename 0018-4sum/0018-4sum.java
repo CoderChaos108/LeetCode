a1ea2a796ce8b9a002ae951c9aa1e66d803fe18d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-06-14 21:48:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        HashSet<ArrayList<Integer>> set=new HashSet<>();
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i:nums){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
+        Arrays.sort(nums);
+        int l=0;
+        int r=0;
+        HashSet<List<Integer>> ans=new HashSet<>(); 
         for(int i=0;i<nums.length-3;i++){
-            hm.put(nums[i],hm.get(nums[i])-1);
-            if(hm.get(nums[i])==0)
-            hm.remove(nums[i]);
             for(int j=i+1;j<nums.length-2;j++){
-                hm.put(nums[j],hm.get(nums[j])-1);
-                if(hm.get(nums[j])==0)
-                hm.remove(nums[j]);
-                for(int k=j+1;k<nums.length-1;k++){
-                    hm.put(nums[k],hm.get(nums[k])-1);
-                    if(hm.get(nums[k])==0)
-                    hm.remove(nums[k]);
-                    long rem=(long)target-(long)nums[i]-(long)nums[j]-(long)nums[k];
-                    if(rem>=Integer.MIN_VALUE&&rem<=Integer.MAX_VALUE&&hm.containsKey((int)rem)){
+                l=j+1;
+                r=nums.length-1;
+                int neotarget=target-nums[i]-nums[j];
+                while(l<r){
+                    if(neotarget>nums[l]+nums[r])
+                    l++;
+                    else if(neotarget<nums[l]+nums[r])
+                    r--;
+                    else{
                         ArrayList<Integer> list=new ArrayList<>();
                         list.add(nums[i]);
                         list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add((int)rem);
-                        Collections.sort(list);
-                        set.add(list);
+                        list.add(nums[l]);
+                        list.add(nums[r]);
+                        ans.add(list);
+                        l++;
+                        r--;
+                        continue;
                     }
-                    hm.put(nums[k],hm.getOrDefault(nums[k],0)+1);
                 }
-                hm.put(nums[j],hm.getOrDefault(nums[j],0)+1);
             }
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        return new ArrayList<>(set);
+        return new ArrayList<>(ans);
     }
 }
