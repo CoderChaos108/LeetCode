2f925d947d5900b45d596b5ed3c7d62d9c1663cb
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-06-18 01:00:43 +0530

    Time: 4 ms (99.57%), Space: 46.4 MB (94.15%) - LeetHub

--- 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        ListNode current=head;
-        int size=0;
-        ListNode prev=null;
-        while(current!=null){
-            ListNode temp=current.next;
-            current.next=prev;
-            prev=current;
-            current=temp;
+        ListNode current = head;
+        int size = 0;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
             size++;
         }
-        int[] ans=new int[size];
-        int i=size-1;
-        ArrayList<Integer> monoStack=new ArrayList<>();
-        while(prev!=null){
-            while(!monoStack.isEmpty()&&monoStack.get(monoStack.size()-1)<=prev.val){
-                monoStack.remove(monoStack.size()-1);
+        int[] ans = new int[size];
+        int i = size - 1;
+        ListNode mono = null; 
+
+        while (prev != null) {
+            while (mono != null && mono.val <= prev.val) {
+                mono = mono.next;
             }
-            if(!monoStack.isEmpty()){
-                ans[i]=monoStack.get(monoStack.size()-1);
+            if (mono != null) {
+                ans[i] = mono.val;
             }
-            monoStack.add(prev.val);
+            ListNode newNode = new ListNode(prev.val);
+            newNode.next = mono;
+            mono = newNode;
             i--;
-            prev=prev.next;
+            prev = prev.next;
         }
         return ans;
     }
