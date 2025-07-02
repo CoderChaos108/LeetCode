8292626d9dd2b72c54395d02c8f0ce98b5681780
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-02 14:06:35 +0530

    Time: 47 ms (26.39%), Space: 56.5 MB (43.06%) - LeetHub

---
+++ 0894-random-pick-with-blacklist/0894-random-pick-with-blacklist.java
@@ -0,0 +1,49 @@
+class Solution {
+    HashMap<Integer,Integer> hm;
+    int k;
+    Random rand;
+    public Solution(int n, int[] a) {
+        Arrays.sort(a);
+        rand=new Random();
+        hm=new HashMap<>();
+        int p=0;
+        int q=a.length-1;
+        if(a.length==1){
+            if(a[0]==n-1)
+            n--;
+            else{
+                hm.put(a[0],n-1);
+                n--;
+            }
+        }
+        else{
+
+        while(p<=q){
+            boolean removed=false;
+            while(p<=q&&a[q]==n-1){
+                n--;
+                q--;
+                removed=true;
+            }
+            if(removed)
+            continue;
+            hm.put(a[p],n-1);
+            p++;
+            n--;
+        }
+        }
+        k=n;
+
+    }
+    
+    public int pick() {
+        int r=(rand.nextInt(k));
+        return hm.getOrDefault(r,r);
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(n, blacklist);
+ * int param_1 = obj.pick();
+ */
