ed6251e52773dec1e3e9362e8c4a6a2fa6614aee
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-03 13:59:29 +0530

    Time: 0 ms (100%), Space: 45.3 MB (66.32%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -14,44 +14,28 @@
  * }
  */
 class Solution {
-    List<Integer> list;
     public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode node=new TreeNode(val);
         if(root==null)
-        return new TreeNode(val);
-        list=new ArrayList<>();
-        inorder(root);
-        for(int i=0;i<list.size();i++){
-            if(list.get(list.size()-1)<=val){
-            list.add(val);
-            break;
-            }
-            if(i==0&&list.get(i)>=val){
-            list.add(0,val);
-            break;
-            }
-            else{
-                if(list.get(i)>=val&&list.get(i-1)<=val){
-                    list.add(i,val);
-                    break;
+        return node;
+        TreeNode current=root;
+        while(true){
+            if(current.val>val){
+                if(current.left==null){
+                    current.left=node;
+                    return root;
+                }
+                else{
+                    current=current.left;
                 }
             }
-        }
-            return build(0,list.size()-1);
-    }
-    public TreeNode build(int l,int r){
-        if(l>r)
-        return null;
-        int m=(l+r)/2;
-        TreeNode node=new TreeNode(list.get(m));
-        node.left=build(l,m-1);
-        node.right=build(m+1,r);
-        return node;
-    }
-    public void inorder(TreeNode node){
-        if(node!=null){
-            inorder(node.left);
-            list.add(node.val);
-            inorder(node.right);
+            else{
+                if(current.right==null){
+                    current.right=node;
+                    return root;
+                }
+                current=current.right;
+            }
         }
     }
 }
