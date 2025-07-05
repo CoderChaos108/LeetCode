fa3a2a88be4f6524a9cc0e53a64cf5ad0040f6f0
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-05 13:40:06 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.11%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,7 +1,11 @@
 class Solution {
     int[] a;
     int len;
+    int[] map;
     public int count(int l,int r){
+        int m=r-l+1;
+        if(map[m]!=0)
+        return map[m];
         if(l==r)
         return 1;
         int s=0;
@@ -9,9 +13,11 @@ class Solution {
         for(int i=l+1;i<=r-1;i++){
             s=s+(count(i+1,r)*count(l,i-1));
         }
+        map[m]=s;
         return s;
     }
     public int numTrees(int n) {
+         map=new int[20];
         len=n-1;
         a=new int[n];
         for(int i=0;i<n;i++)
