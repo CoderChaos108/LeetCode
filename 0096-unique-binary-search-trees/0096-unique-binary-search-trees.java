89efebd9ab7f6724f4cce07d4d2c7ff52ba06ccc
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-05 13:36:52 +0530

    Time: 1283 ms (10.72%), Space: 40.7 MB (26%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,21 +1,18 @@
 class Solution {
     int[] a;
+    int len;
     public int count(int l,int r){
         if(l==r)
         return 1;
         int s=0;
-        for(int i=l;i<=r;i++){
-            int left=1;
-            if(i!=l)
-            left=count(l,i-1);
-            int right=1;
-            if(i!=r)
-            right=count(i+1,r);
-            s=s+(right*left);
+        s=s+count(l+1,r)+count(l,r-1);
+        for(int i=l+1;i<=r-1;i++){
+            s=s+(count(i+1,r)*count(l,i-1));
         }
         return s;
     }
     public int numTrees(int n) {
+        len=n-1;
         a=new int[n];
         for(int i=0;i<n;i++)
         a[i]=i+1;
