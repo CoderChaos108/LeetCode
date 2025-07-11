9c63330c69eabb003f6baf75bee416b8e49fd564
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-11 15:47:01 +0530

    Time: 47 ms (88.65%), Space: 62.5 MB (59.06%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
@@ -1,29 +1,27 @@
 class Solution {
-    HashMap<Integer,List<Integer>> hm;
+    List<Integer>[] a;
     int[] t;
     public int time(int id){
-        if(!hm.containsKey(id))
+        if(a[id].size()==0)
         return 0;
         int time=t[id];
         int s=0;
-        for(int i:hm.get(id)){
+        for(int i:a[id]){
             s=Math.max(s,time(i));
         }
         return time+s;
     }
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         t=informTime;
-        hm=new HashMap<>();
+        a=new ArrayList[n];
         for(int i=0;i<n;i++){
+            a[i]=new ArrayList<>();
+        }
+        for(int i=0;i<n;i++){
+            if(i==headID)
+            continue;
             int m=manager[i];
-            if(hm.containsKey(m)){
-                hm.get(m).add(i);
-            }
-            else{
-                List<Integer> list=new ArrayList<>();
-                list.add(i);
-                hm.put(m,list);
-            }
+            a[m].add(i);
         }
         return time(headID);
     }
