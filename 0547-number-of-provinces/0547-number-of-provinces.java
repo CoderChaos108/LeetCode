8467f913b920182460a1643cc33c3729513af34e
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-17 04:57:23 +0530

    Time: 2 ms (46.3%), Space: 48.2 MB (20.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -12,9 +12,10 @@ class Solution {
         return 0;
     }
     public int findCircleNum(int[][] isConnected) {
+        int l=isConnected.length;
         seen=new HashSet<>(); 
         int c=0;
-        for(int i=0;i<isConnected.length;i++)
+        for(int i=0;i<l;i++)
         c=c+check(i,isConnected);
         return c;
     }
