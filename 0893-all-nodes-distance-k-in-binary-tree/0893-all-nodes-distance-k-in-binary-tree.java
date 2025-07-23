2e4f26ce4e32040910650be78d03159c208bfeb9
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-24 01:59:12 +0530

    Time: 11 ms (97.56%), Space: 42.7 MB (48.95%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -1,47 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    HashMap<TreeNode,List<TreeNode>> hm;
-    TreeNode t;
-    List<Integer> ans;
+    HashMap<TreeNode,TreeNode> hm;
     HashSet<TreeNode> set;
+    List<Integer> ans;
+    TreeNode t;
     int k;
+    
     public void dfs(TreeNode node,TreeNode parent){
-        if(node==null)
-        return;
-        List<TreeNode> list=new ArrayList<>();
-        list.add(parent);
-        list.add(node.left);
-        list.add(node.right);
-        hm.put(node,list);
+        if(node==null)return;
+        if(parent!=null)hm.put(node,parent);
         dfs(node.left,node);
         dfs(node.right,node);
     }
+    
     public void build(TreeNode node,int d){
-        if(node==null||set.contains(node)||d>k)
+        if(node==null||set.contains(node))
         return;
         set.add(node);
         if(d==k){
             ans.add(node.val);
             return;
         }
-        for(TreeNode n:hm.get(node)){
-            build(n,d+1);
-        }
+        build(node.left,d+1);
+        build(node.right,d+1);
+        build(hm.get(node),d+1);
     }
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int find) {
         hm=new HashMap<>();
-        t=target;       
-        k=find;
-        ans=new ArrayList<>();
         set=new HashSet<>();
+        ans=new ArrayList<>();
+        t=target;
+        k=find;
         dfs(root,null);
         build(t,0);
         return ans;
