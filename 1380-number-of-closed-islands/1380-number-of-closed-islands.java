cbe0b62821d0a5c9483c90e1fb23eb028510e8f1
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-07-27 22:28:08 +0530

    Time: 2 ms (100%), Space: 44.5 MB (69.25%) - LeetHub

---
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[][] a;
+    int m,n;
+    public void dfs(int i,int j){
+        if(i<0||j<0||i>=m||j>=n||a[i][j]==1)
+        return;
+        a[i][j]=1;
+        dfs(i+1,j);
+        dfs(i-1,j);
+        dfs(i,j+1);
+        dfs(i,j-1);
+    }
+
+    public int closedIsland(int[][] grid) {
+        a=grid;
+        m=a.length;
+        n=a[0].length;
+        for(int i=0;i<m;i++){
+            dfs(i,0);
+            dfs(i,n-1);
+        }
+        for(int j=0;j<n;j++){
+            dfs(0,j);
+            dfs(m-1,j);
+        }
+        int c=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(a[i][j]==0){
+                    dfs(i,j);
+                    c++;
+                }
+
+            }
+        }
+        return c;
+    }
+}
