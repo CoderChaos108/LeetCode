2e6ff15df8be7ef5a17e508d7615733a4cd69040
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-03 18:49:16 +0530

    Time: 61 ms (5.18%), Space: 98.9 MB (5.36%) - LeetHub

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,34 +1,30 @@
 class Solution {
     List<List<Integer>> graph=new ArrayList<>();
-    boolean[] seen,set;
+    boolean[] seen;
+    HashSet<Integer> set=new HashSet<>();
+    
     public void check(int node){
         if(seen[node]){
-        set[node]=false;
-        return;
+            set.remove(node);
+            return;
         }
         seen[node]=true;
         for(int i:graph.get(node))
-        check(i);
+            check(i);
     }
-    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+    
+    public List<Integer> findSmallestSetOfVertices(int n,List<List<Integer>> edges){
         seen=new boolean[n];
-        set=new boolean[n];
         for(int i=0;i<n;i++)
-        graph.add(new ArrayList<>());
-        for(List<Integer> a:edges){
+            graph.add(new ArrayList<>());
+        for(List<Integer> a:edges)
             graph.get(a.get(0)).add(a.get(1));
-        }
         for(int i=0;i<n;i++){
             if(!seen[i]){
-            set[i]=true;
-            check(i);
+                set.add(i);
+                check(i);
             }
         }
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(set[i])
-            list.add(i);
-        }
-        return list;
+        return new ArrayList<>(set);
     }
 }
