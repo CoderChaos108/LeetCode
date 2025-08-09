c7963e91fe4dff5cf37844ae3a4586d33643c61d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-10 01:47:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,23 +1,10 @@
 class Solution {
-    static boolean recursive(int x){
-        if(x%2!=0){
-        if(x!=1)
-        return false;
-        if(x==1)
-        return true;
-        }
-            return recursive(x/2);
-        
-    }
     public boolean isPowerOfTwo(int n) {
-        if(n==1)
+        while(n!=2&&n!=1){
+            n=n<<1;
+        }
+        if(n==2)
         return true;
-        if(n<1)
         return false;
-        if(n%2!=0)
-        return false;
-        if(n%3==0||n%5==0||n%7==0)
-        return false;
-        return recursive(n);
     }
 }
