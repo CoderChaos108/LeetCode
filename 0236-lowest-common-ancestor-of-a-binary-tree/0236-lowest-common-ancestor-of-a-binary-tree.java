bb8aa5a3e46792375332f3b0218060101d4fa273
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-12 19:17:08 +0530

    Time: 6 ms (100%), Space: 44.6 MB (85.14%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,49 +8,22 @@
  * }
  */
 class Solution {
-    List<TreeNode> preOrder=new ArrayList<>();
-    public void dfs(TreeNode node){
-        if(node==null)
-        return;
-        preOrder.add(node);
-        dfs(node.left);
-        dfs(node.right);
-    }
     TreeNode n1,n2;
-    boolean a,b=false;
-    public boolean search(TreeNode node){
-        if(node==null)
-        return false;
-        if(node==n1)
-        a=true;
-        if(node==n2)
-        b=true;
-        if(a&&b)
-        return true;
-        return search(node.left)||search(node.right);
+    public TreeNode LCA(TreeNode node){
+        if(node==null||node==n1||node==n2)
+        return node;
+        TreeNode left=LCA(node.left);
+        TreeNode right=LCA(node.right);
+        if(left!=null&&right!=null)
+        return node;
+        if(left==null)
+        return right;
+        else
+        return left;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        n1=p;
-        n2=q;
-        dfs(root);
-        boolean foundP=false;
-        boolean foundQ=false;
-        int i=preOrder.size()-1;
-        while(!foundP||!foundQ){
-            TreeNode node=preOrder.get(i);
-            if(node==p)
-            foundP=true;
-            if(node==q)
-            foundQ=true;
-            i--;
-        }
-        i++;
-        for(int j=i;j>0;j--){
-            if(search(preOrder.get(j)))
-            return preOrder.get(j);
-            a=false;
-            b=false;
-        }
-        return root;
+     n1=p;
+     n2=q;
+     return LCA(root);   
     }
 }
