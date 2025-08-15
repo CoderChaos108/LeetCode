24f21a49925a2371adb14ce3e4d09179103e6699
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-15 19:33:59 +0530

    Time: 90 ms (11.61%), Space: 45.4 MB (53.4%) - LeetHub

---
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.add(new int[] {0,0,0});
+        int m=heights.length;
+        int n=heights[0].length;
+        boolean[][] seen=new boolean[m][n];
+        while(!pq.isEmpty()){
+            int[] a=pq.poll();
+            int eff=a[0];
+            int i=a[1];
+            int j=a[2];
+            seen[i][j]=true;
+            if(i==m-1&&j==n-1)
+            return eff;
+            int val=heights[i][j];
+            if(i-1>=0&&!seen[i-1][j]){
+                int newEff=Math.max(eff,Math.abs(val-heights[i-1][j]));
+                pq.add(new int[]{newEff,i-1,j});
+            }
+            if(i+1<m&&!seen[i+1][j]){
+                int newEff=Math.max(eff,Math.abs(val-heights[i+1][j]));
+                pq.add(new int[]{newEff,i+1,j});
+            }
+            if(j-1>=0&&!seen[i][j-1]){
+                int newEff=Math.max(eff,Math.abs(val-heights[i][j-1]));
+                pq.add(new int[]{newEff,i,j-1});
+            }
+            if(j+1<n&&!seen[i][j+1]){
+                int newEff=Math.max(eff,Math.abs(val-heights[i][j+1]));
+                pq.add(new int[]{newEff,i,j+1});
+            }
+        }
+        return 0;
+    }
+}
