abe713aedef31e0fb79df4f104ccad54df1f9d58
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-17 20:33:57 +0530

    Time: 40 ms (17.32%), Space: 45.1 MB (18.39%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,46 +1,46 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        List<List<List<Integer>>> graph=new ArrayList<>();
+        List<List<int[]>> graph=new ArrayList<>();
         int m=Integer.MAX_VALUE;
         int ans=0;
         int k=distanceThreshold;
         for(int i=0;i<n;i++){
-        graph.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         for(int i=0;i<edges.length;i++){
             int x=edges[i][0];
             int y=edges[i][1];
             int w=edges[i][2];
-            graph.get(y).add(Arrays.asList(w,x));
-            graph.get(x).add(Arrays.asList(w,y));
+            graph.get(y).add(new int[]{w,x});
+            graph.get(x).add(new int[]{w,y});
         }
         for(int i=0;i<n;i++){
-            PriorityQueue<List<Integer>> pq=new PriorityQueue<>((a,b)->a.get(0)-b.get(0));
+            PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
             int[] a=new int[n];
             for(int j=0;j<n;j++)
-            a[j]=Integer.MAX_VALUE;
+                a[j]=Integer.MAX_VALUE;
             a[i]=0;
-            pq.add(Arrays.asList(0,i));
+            pq.add(new int[]{0,i});
             int cities=-1;
             while(!pq.isEmpty()){
-                List<Integer> list=pq.poll();
-                int dis=list.get(0);
-                int node=list.get(1);
+                int[] arr=pq.poll();
+                int dis=arr[0];
+                int node=arr[1];
                 if(dis>k)
-                continue;
-                for(List<Integer> neigh:graph.get(node)){
-                    int newNode=neigh.get(1);
-                    int newVal=neigh.get(0)+dis;
+                    continue;
+                for(int[] neigh:graph.get(node)){
+                    int newNode=neigh[1];
+                    int newVal=neigh[0]+dis;
                     if(newVal<a[newNode]&&newVal<=k){
                         a[newNode]=newVal;
-                        pq.add(Arrays.asList(newVal,newNode));
-                        }
+                        pq.add(new int[]{newVal,newNode});
                     }
                 }
-                for(int j=0;j<n;j++){
-                    if(a[j]<=k)
+            }
+            for(int j=0;j<n;j++){
+                if(a[j]<=k)
                     cities++;
-                }
+            }
             if(m>=cities){
                 m=cities;
                 ans=i;
