17fa99e1e12a3495618139be1e1dc6e9cc9c502a
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-21 03:49:02 +0530

    Time: 13 ms (48.4%), Space: 49.1 MB (30.43%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,38 +1,24 @@
 class Solution {
-    List<List<int[]>> graph=new ArrayList<>();
-    int[] distance;
-    public int networkDelayTime(int[][] times,int n,int k) {
-        distance=new int[n+1];
-        for(int i=0;i<=n;i++) {
-            graph.add(new ArrayList<>());
-            distance[i]=Integer.MAX_VALUE;
-        }
-        distance[k]=0;
-        for(int i=0;i<times.length;i++) {
-            List<int[]> list=graph.get(times[i][0]);
-            list.add(new int[]{times[i][1],times[i][2]});
-        }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        pq.add(new int[]{0,k});
-        while(!pq.isEmpty()) {
-            int[] l=pq.poll();
-            int d=l[0];
-            int current=l[1];
-            if(d>distance[current]) continue;
-            for(int[] list:graph.get(current)) {
-                int node=list[0];
-                int curDis=d+list[1];
-                if(distance[node]>curDis) {
-                    distance[node]=curDis;
-                    pq.add(new int[]{curDis,node});
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=Integer.MAX_VALUE;
+        a[k-1]=0;
+        for(int i=0;i<n-1;i++){
+            for(int[] arr:times){
+                int x=arr[0]-1;
+                int y=arr[1]-1;
+                int w=arr[2];
+                if(a[x]!=Integer.MAX_VALUE&&w+a[x]<a[y]){
+                    a[y]=w+a[x];
                 }
             }
         }
         int ans=-1;
-        for(int i=1;i<=n;i++) {
-            if(distance[i]==Integer.MAX_VALUE)
+        for(int i:a){
+            if(i==Integer.MAX_VALUE)
             return -1;
-            ans=Math.max(ans,distance[i]);
+            ans=Math.max(ans,i);
         }
         return ans;
     }
