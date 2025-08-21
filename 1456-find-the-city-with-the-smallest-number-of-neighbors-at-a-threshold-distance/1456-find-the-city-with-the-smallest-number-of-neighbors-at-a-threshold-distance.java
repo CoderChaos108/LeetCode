d9fc433444922628604f67f4950866fdc6e19023
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-21 20:48:30 +0530

    Time: 11 ms (41.24%), Space: 44.5 MB (59.74%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,49 +1,41 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        List<List<int[]>> graph=new ArrayList<>();
-        int m=Integer.MAX_VALUE;
-        int ans=0;
-        int k=distanceThreshold;
+        int[][] a=new int[n][n];
         for(int i=0;i<n;i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int i=0;i<edges.length;i++){
-            int x=edges[i][0];
-            int y=edges[i][1];
-            int w=edges[i][2];
-            graph.get(y).add(new int[]{w,x});
-            graph.get(x).add(new int[]{w,y});
-        }
-        for(int i=0;i<n;i++){
-            PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-            int[] a=new int[n];
-            for(int j=0;j<n;j++)
-                a[j]=Integer.MAX_VALUE;
-            a[i]=0;
-            pq.add(new int[]{0,i});
-            int cities=-1;
-            while(!pq.isEmpty()){
-                int[] arr=pq.poll();
-                int dis=arr[0];
-                int node=arr[1];
-                if(dis>k)
-                    continue;
-                for(int[] neigh:graph.get(node)){
-                    int newNode=neigh[1];
-                    int newVal=neigh[0]+dis;
-                    if(newVal<a[newNode]&&newVal<=k){
-                        a[newNode]=newVal;
-                        pq.add(new int[]{newVal,newNode});
-                    }
-                }
-            }
             for(int j=0;j<n;j++){
-                if(a[j]<=k)
-                    cities++;
+                if(i!=j)
+                a[i][j]=Integer.MAX_VALUE;
             }
-            if(m>=cities){
-                m=cities;
+        }
+        for(int arr[]:edges){
+            int x=arr[0];
+            int y=arr[1];
+            int w=arr[2];
+            a[x][y]=w;
+            a[y][x]=w;
+        }
+        for(int k=0;k<n;k++){
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==k||k==j)
+                continue;
+                // i j>ik+kj
+                if(a[i][k]!=Integer.MAX_VALUE&&a[k][j]!=Integer.MAX_VALUE)
+                a[i][j]=Math.min(a[i][j],(a[i][k]+a[k][j]));
+            }
+         }   
+        }
+        int min=Integer.MAX_VALUE;
+        int ans=-1;
+        for(int i=0;i<n;i++){
+            int neigh=0;
+            for(int j=0;j<n;j++){
+                if(a[i][j]<=distanceThreshold)
+                neigh++;
+            }
+            if(neigh<=min){
                 ans=i;
+                min=neigh;
             }
         }
         return ans;
