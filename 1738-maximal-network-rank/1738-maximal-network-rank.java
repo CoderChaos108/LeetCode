2306ec125cb21c00846454ec73513fb99c333075
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-23 01:05:05 +0530

    Time: 9 ms (54.32%), Space: 46.1 MB (20.25%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.java
+++ 1738-maximal-network-rank/1738-maximal-network-rank.java
@@ -1,19 +1,29 @@
 class Solution {
-    List<HashSet<Integer>> graph=new ArrayList<>();
+    int total;
+    public class Node{
+        int size=0;
+        boolean[] set=new boolean[total]; 
+    }
+    List<Node> graph=new ArrayList<>();
     public int maximalNetworkRank(int n, int[][] roads) {
+        total=n;
         for(int i=0;i<n;i++)
-        graph.add(new HashSet<>());
+        graph.add(new Node());
         for(int[] a:roads){
             int c1=a[0];
             int c2=a[1];
-            graph.get(c1).add(c2);
-            graph.get(c2).add(c1);
+            Node n1=graph.get(c1);
+            Node n2=graph.get(c2);
+            n1.set[c2]=true;
+            n1.size++;
+            n2.size++;
+            n2.set[c1]=true;
         }
         int m=0;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                int rank=graph.get(i).size()+graph.get(j).size();
-                if(graph.get(i).contains(j))
+                int rank=graph.get(i).size+graph.get(j).size;
+                if(graph.get(i).set[j])
                 rank--;
                 m=Math.max(m,rank);
             }
