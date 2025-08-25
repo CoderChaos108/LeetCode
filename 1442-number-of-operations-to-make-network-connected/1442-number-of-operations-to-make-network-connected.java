228cfec248210449560933bbf25f0fb7a87f81d4
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-26 02:40:09 +0530

    Time: 13 ms (37.84%), Space: 71.5 MB (9.49%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -2,18 +2,19 @@ class Solution {
     boolean[] seen;
     List<List<Integer>> graph=new ArrayList<>();
     int cables=0;
-    public void dfs(int node,int parent){
+    public void dfs(int node){
         if(seen[node]){
-        cables++;
         return;
         }
         seen[node]=true;
         for(int neigh:graph.get(node)){
-        if(neigh!=parent)
-        dfs(neigh,node);
+        dfs(neigh);
         }
     }
     public int makeConnected(int n, int[][] connections) {
+        cables=connections.length-n+1;
+        if(cables<0)
+        return -1;
         for(int i=0;i<n;i++)
         graph.add(new ArrayList<>());
         for(int[] a:connections){
@@ -27,11 +28,9 @@ class Solution {
             for(int i=0;i<n;i++){
                 if(!seen[i]){
                     c++;
-                    dfs(i,-1);
+                    dfs(i);
                 }
             }
-            if(cables/2<c)
-            return -1;
             return c;
     }
 }
