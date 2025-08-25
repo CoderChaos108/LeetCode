99ed3b9dc775d8072459396cf1867a5aaa81b6fd
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-26 03:14:30 +0530

    Time: 36 ms (78.51%), Space: 73.7 MB (46.11%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,36 +1,34 @@
 class Solution {
-    List<List<List<Integer>>> graph=new ArrayList<>();
+    List<List<Integer>> graph=new ArrayList<>();
     boolean[] seen;
     int c=0;
+
     public void dfs(int node){
         if(seen[node])
         return;
         seen[node]=true;
-        for(int out:graph.get(node).get(0)){
-            if(!seen[out]){
-                c++;
-                dfs(out);
+
+        for(int next:graph.get(node)){
+            if(!seen[Math.abs(next)]){
+                if(next>0)
+                    c++;
+                dfs(Math.abs(next));
             }
         }
-            for(int in:graph.get(node).get(1)){
-                if(!seen[in])
-                dfs(in);
-            }
     }
+
     public int minReorder(int n, int[][] connections) {
         seen=new boolean[n];
-        for(int i=0;i<n;i++){
-            List<List<Integer>> list=new ArrayList<>();
-            list.add(new ArrayList<>());
-            list.add(new ArrayList<>());
-            graph.add(list);
-        }
+        for(int i=0;i<n;i++)
+            graph.add(new ArrayList<>());
+
         for(int[] a:connections){
             int x=a[0];
             int y=a[1];
-            graph.get(x).get(0).add(y);
-            graph.get(y).get(1).add(x);
+            graph.get(x).add(+y);
+            graph.get(y).add(-x);
         }
+
         dfs(0);
         return c;
     }
