2a132f889294b88bf9c0025ce8a3fc3be31eaae3
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-26 01:40:00 +0530

    Time: 2 ms (22.47%), Space: 42.4 MB (17.91%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.java
@@ -1,51 +1,41 @@
 class Solution {
-    List<List<Integer>> graph=new ArrayList<>();
-    List<String> queries=new ArrayList<>();
-    boolean[] seen=new boolean[26];
-    HashSet<List<Integer>> set=new HashSet<>();
-    List<Integer> list;
+    boolean[][] graph=new boolean[26][26];
+    boolean[] seen;
+
     public boolean check(int x,int y){
-        if(seen[x]||set.contains(list))
-        return true;
-        seen[x]=true;
         if(x==y)
-        return false;
-        for(int neigh:graph.get(x)){
-        if(!check(neigh,y))
-        return false;
-        }
         return true;
+        if(seen[x])
+        return false;
+        seen[x]=true;
+        for(int neigh=0;neigh<26;neigh++){
+            if(graph[x][neigh]&&check(neigh,y))
+            return true;
+        }
+        return false;
     }
-    public boolean equationsPossible(String[] equations) {
-        for(int i=0;i<26;i++)
-            graph.add(new ArrayList<>());
+
+    public boolean equationsPossible(String[] equations){
         for(String s:equations){
-            if(s.charAt(1)=='!')
-            queries.add(s);
-            else{
+            if(s.charAt(1)=='='){
                 int x=s.charAt(0)-'a';
                 int y=s.charAt(3)-'a';
                 if(x==y)
-                continue;
-                graph.get(x).add(y);
-                graph.get(y).add(x);
+                 continue;
+                graph[x][y]=true;
+                graph[y][x]=true;
             }
         }
-        for(String s:queries){
-             seen=new boolean[26];
-            int x=s.charAt(0)-'a';
-            int y=s.charAt(3)-'a';
-            int sizeX=graph.get(x).size();
-            int sizeY=graph.get(y).size();
-            list=Arrays.asList(x,y);
-            Collections.sort(list);
-            if(x==y)
-            return false;
-            if(sizeX==0||sizeY==0)
-            continue;
-            if(!check(x,y))
-            return false;
-           set.add(list);
+        for(String s:equations){
+            if(s.charAt(1)=='!'){
+                int x=s.charAt(0)-'a';
+                int y=s.charAt(3)-'a';
+                if(x==y) 
+                return false;
+                seen=new boolean[26];
+                if(check(x,y)) 
+                return false;
+            }
         }
         return true;
     }
