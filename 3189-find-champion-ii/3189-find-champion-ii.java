4f318bf9fb96850cf3f33fb70f8ca5834aa40ae0
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-08-31 23:02:56 +0530

    Time: 1 ms (100%), Space: 46.2 MB (19.23%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,25 +1,9 @@
 class Solution {
-    List<List<Integer>> graph=new ArrayList<>();
     boolean[] seen;
-    public void dfs(int node){
-        if(seen[node])
-        return;
-        seen[node]=true;
-        for(int neigh:graph.get(node))
-        dfs(neigh);
-    }
     public int findChampion(int n, int[][] edges) {
         seen=new boolean[n];
-        for(int i=0;i<n;i++)
-        graph.add(new ArrayList<>());
         for(int[] a:edges){
-            graph.get(a[0]).add(a[1]);
-        }
-        for(int i=0;i<n;i++){
-            if(!seen[i]){
-            dfs(i);
-            seen[i]=false;
-            }
+            seen[a[1]]=true;
         }
         int champ=-1;
         for(int i=0;i<n;i++){
