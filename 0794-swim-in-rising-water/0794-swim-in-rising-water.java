cf7692b3ae222ebd291fd098a9de10bc5b9cec70
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-09-06 19:43:58 +0530

    Time: 9 ms (85.89%), Space: 45.4 MB (5.23%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,35 +1,56 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int n=grid[0].length;
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        pq.add(new int[]{grid[0][0],0,0});
-        boolean[][] seen=new boolean[n][n];
-        while(true){
-            int[] a=pq.poll();
-            int t=a[0];
-            int i=a[1];
-            int j=a[2];
-            if(seen[i][j])
-            continue;
-            seen[i][j]=true;
-            if(i==n-1&&j==n-1)
-            return t;
-            if(i-1>=0){
-                int newT=Math.max(grid[i-1][j],t);
-                pq.add(new int[]{newT,i-1,j});
-            }
-            if(j-1>=0){
-                int newT=Math.max(grid[i][j-1],t);
-                pq.add(new int[]{newT,i,j-1});
-            }
-            if(i+1<n){
-                int newT=Math.max(grid[i+1][j],t);
-                pq.add(new int[]{newT,i+1,j});
-            }
-            if(j+1<n){
-                int newT=Math.max(grid[i][j+1],t);
-                pq.add(new int[]{newT,i,j+1});
+        int n=grid.length;
+        int max=-1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                max=Math.max(max,grid[i][j]);
             }
         }
+        int min=Math.max(grid[0][0],grid[n-1][n-1]);
+        
+        while(min<max){
+            int m=(min+max)/2;
+            if(canReach(grid,m,n)){
+                max=m;
+            }else{
+                min=m+1;
+            }
+        }
+        return min;
+    }
+    
+    private boolean canReach(int[][] grid,int m,int n){
+        Queue<int[]> q=new LinkedList<>();
+        boolean[][] seen=new boolean[n][n];
+        q.add(new int[]{0,0});
+        seen[0][0]=true;
+        
+        while(!q.isEmpty()){
+            int[] a=q.poll();
+            int i=a[0];
+            int j=a[1];
+            if(i==n-1&&j==n-1){
+                return true;
+            }
+            if(grid[i][j]>m) continue;
+            if(i-1>=0&&!seen[i-1][j]){
+                seen[i-1][j]=true;
+                q.add(new int[]{i-1,j});
+            }
+            if(i+1<n&&!seen[i+1][j]){
+                seen[i+1][j]=true;
+                q.add(new int[]{i+1,j});
+            }
+            if(j-1>=0&&!seen[i][j-1]){
+                seen[i][j-1]=true;
+                q.add(new int[]{i,j-1});
+            }
+            if(j+1<n&&!seen[i][j+1]){
+                seen[i][j+1]=true;
+                q.add(new int[]{i,j+1});
+            }
+        }
+        return false;
     }
 }
