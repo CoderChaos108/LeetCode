bb92036c62ff7ae52574300556ee71b2f6c54cfb
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-09-14 19:04:35 +0530

    Time: 5 ms (7.48%), Space: 45.3 MB (5.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,33 +2,28 @@ class Solution {
     List<List<Integer>> ans=new ArrayList<>();
     int[] a;
     int target;
-    HashSet<List<Integer>> set=new HashSet<>();
-    public void find(List<Integer> list,int sum){
+    public void find(List<Integer> list,int sum,int k){
         if(sum==target){
-            Collections.sort(list);
-            if(set.contains(list))
-            return;
-            set.add(list);
             ans.add(list);
             return;
         }
         if(sum>target)
         return;
         else{
-            for(int i:a){
+            for(int i=k;i<a.length;i++){
                 List<Integer> list2=new ArrayList<>(list);
-                list2.add(i);
-                find(list2,sum+i);
+                list2.add(a[i]);
+                find(list2,sum+a[i],i);
             }
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int t) {
         a=candidates;
         target=t;
-        for(int i:a){
+        for(int i=0;i<a.length;i++){
             List<Integer> list=new ArrayList<>();
-            list.add(i);
-            find(list,i);
+            list.add(a[i]);
+            find(list,a[i],i);
         }
         return ans;
     }
