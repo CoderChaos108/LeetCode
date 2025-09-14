3d8d16c3816d51725d406e2fb814f28261f671a8
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-09-14 21:19:40 +0530

    Time: 32 ms (19.93%), Space: 45.5 MB (13.25%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,26 +1,27 @@
 class Solution {
-    HashSet<List<Integer>> ans=new HashSet<>();
+    Set<List<Integer>> ans=new HashSet<>();
     int[] a;
     int n;
-    HashSet<Integer> set=new HashSet<>();
+    boolean[] seen;
     public void find(List<Integer> list,int k){
         if(list.size()==n){
             ans.add(new ArrayList<>(list));
             return;
         }
         for(int i=0;i<n;i++){
-            if(set.contains(i))
+            if(seen[i])
             continue;
-            set.add(i);
+            seen[i]=true;
             list.add(a[i]);
             find(list,i);
             list.remove(list.size()-1);
-            set.remove(i);
+            seen[i]=false;
         }
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
         a=nums;
         n=a.length;
+        seen=new boolean[n];
         find(new ArrayList<>(),-1);
         return new ArrayList<>(ans);
     }
