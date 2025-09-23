1d91be47fb6fa51c6d51e2c5bf3c48f0fd7e4d93
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-09-23 19:00:56 +0530

    Time: 48 ms (19.11%), Space: 40.1 MB (94.36%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -2,21 +2,18 @@ class Solution {
     int n;
     boolean[] seen;
     int ans=0;
-    public void find(List<Integer> list){
-        if(list.size()==n){
+    public void find(int size){
+        if(size==n){
             ans++;
             return;
         }
         for(int i=1;i<=n;i++){
             if(seen[i-1])
             continue;
-            int size=list.size()+1;
-            if(i%size!=0&&size%i!=0)
+            if(i%(size+1)!=0&&(size+1)%i!=0)
             continue;
-            list.add(i);
             seen[i-1]=true;
-            find(list);
-            list.remove(list.size()-1);
+            find(size+1);
             seen[i-1]=false;
         }
     }
@@ -24,7 +21,7 @@ class Solution {
     public int countArrangement(int n2) {
         n=n2;
         seen=new boolean[n];
-        find(new ArrayList<>());
+        find(0);
         return ans;
     }
 }
