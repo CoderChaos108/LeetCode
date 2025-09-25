feac54f0b100541b8031bb926795210d771b3db8
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-09-25 17:09:44 +0530

    Time: 9 ms (11.07%), Space: 46.6 MB (7%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,17 +1,21 @@
 class Solution {
     List<List<Integer>> list;
-    HashMap<List<Integer>,Integer> hm=new HashMap<>();
+    HashMap<String,Integer> hm=new HashMap<>();
     public int dfs(int i,int j){
         if(j>=list.get(i).size())
         return Integer.MIN_VALUE;
         int s=list.get(i).get(j);
         if(i==list.size()-1)
         return s;
-        List<Integer> l=Arrays.asList(i,j);
-        if(hm.containsKey(l))
-        return hm.get(l);
+        StringBuilder sb=new StringBuilder();
+        sb.append(String.valueOf(i));
+        sb.append(",");
+        sb.append(String.valueOf(j));
+        String str=sb.toString();
+        if(hm.containsKey(str))
+        return hm.get(str);
         s=s+Math.min(dfs(i+1,j),dfs(i+1,j+1));
-        hm.put(l,s);
+        hm.put(str,s);
         return s;
     }
     public int minimumTotal(List<List<Integer>> triangle) {
