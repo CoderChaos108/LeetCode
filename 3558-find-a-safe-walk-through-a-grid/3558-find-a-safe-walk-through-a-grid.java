516ddf980551104ff9ec21ac0668c63115890937
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-05 18:35:45 +0530

    Time: 36 ms (35.03%), Space: 45.3 MB (39.12%) - LeetHub

---
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[2]-a[2]);
+        pq.add(new int[]{0,0,health-grid.get(0).get(0)});
+        int m=grid.size();
+        int n=grid.get(0).size();
+        boolean[][] seen=new boolean[m][n];
+        while(true){
+            int[] a=pq.poll();
+            int i=a[0];
+            int j=a[1];
+            if(seen[i][j])
+            continue;
+            seen[i][j]=true;
+            int hp=a[2];
+            if(i==m-1&&j==n-1){
+                if(hp>=1)
+                return true;
+                return false;
+            }
+            if(i-1>=0){
+    pq.add(new int[]{i-1,j,hp-grid.get(i-1).get(j)});
+            }
+            if(j-1>=0){
+                pq.add(new int[]{i,j-1,hp-grid.get(i).get(j-1)});
+            }
+            if(i+1<m){
+                pq.add(new int[]{i+1,j,hp-grid.get(i+1).get(j)});
+            }
+            if(j+1<n){
+                pq.add(new int[]{i,j+1,hp-grid.get(i).get(j+1)});
+            }
+
+        }
+    }
+}
