3c46c7f224a398825b1d30f06cb6b29a54b5fd0e
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-10 15:34:30 +0530

    Time: 4 ms (100%), Space: 60.8 MB (50.13%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -12,27 +12,31 @@ class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         ListNode l=head;
         ListNode m=head.next;
-        ListNode r=head.next.next;
-        int itr=1;
-        List<Integer> list=new ArrayList<>();
-        int min=Integer.MAX_VALUE;
+        ListNode r=head.next.next;        
+        int itr=1;        
+        int firstItr=-1;        
+        int lastItr=-1;         
+        int min=Integer.MAX_VALUE;   
         while(r!=null){
             int lv=l.val;
             int mv=m.val;
             int rv=r.val;
             if((lv<mv&&rv<mv)||(lv>mv&&rv>mv)){
-            list.add(itr);
-            if(list.size()>=2){
-                min=Math.min(min,itr-list.get(list.size()-2));
+                if(firstItr==-1){
+                    firstItr=itr;
+                } else {
+                    min=Math.min(min,itr-lastItr);
+                }
+                lastItr=itr;
             }
-            }
+            
             l=l.next;
             m=m.next;
             r=r.next;
             itr++;
-        }
-        if(list.size()<2)
-        return new int[]{-1,-1};
-        return new int[]{min,list.get(list.size()-1)-list.get(0)};
+        }      
+        if(firstItr==-1||firstItr==lastItr)
+        return new int[]{-1,-1};     
+        return new int[]{min,lastItr-firstItr};
     }
 }
