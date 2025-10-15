37eee90b43438ffbea234ff4df7a2da7e9e29feb
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-15 21:11:44 +0530

    Time: 0 ms (100%), Space: 42.6 MB (43.24%) - LeetHub

--- 2080-check-if-move-is-legal/2080-check-if-move-is-legal.java
+++ 2080-check-if-move-is-legal/2080-check-if-move-is-legal.java
@@ -3,14 +3,12 @@ class Solution {
         int[][] dir=new int[][]{
             {-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}
         };
-        board[rMove][cMove]=color;
         for(int[] a:dir){
             int ni=a[0];
             int nj=a[1];
-            int i=rMove;
-            int j=cMove;
+            int i=rMove+ni;
+            int j=cMove+nj;
             boolean oppFound=false;
-            boolean first=true;
             while(i>=0&&j>=0&&i<8&&j<8){
                 char c=board[i][j];
                 if(c=='.')
@@ -18,9 +16,7 @@ class Solution {
                 if(c==color){
                     if(oppFound)
                     return true;
-                    if(!first)
                     break;
-                    first=false;
                 }
                 else
                 oppFound=true;
