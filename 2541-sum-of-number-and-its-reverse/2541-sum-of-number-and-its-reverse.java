38dc91f7ae0025c776aa9bb7b27aca27b38bd845
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-16 21:35:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2541-sum-of-number-and-its-reverse/2541-sum-of-number-and-its-reverse.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int n){
+        int rev=0;
+        while(n!=0){
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        return rev;
+    }
+    public boolean sumOfNumberAndReverse(int num) {
+        for(int i=0;i<num;i++){
+            if(i+reverse(i)==num)
+            return true;
+        }
+        return false;
+    }
+}
