b8f97d3ca402e6ef60b8b36745cf8d9d41f7144a
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-19 20:19:51 +0530

    Time: 162 ms (11.24%), Space: 55.3 MB (5.62%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
@@ -1,33 +1,27 @@
 class Solution {
     HashSet<String> seen=new HashSet<>();
+    public String check(String s1,String s2){
+        int size=Math.min(s1.length(),s2.length());
+        for(int i=0;i<size;i++){
+            char c1=s1.charAt(i);
+            char c2=s2.charAt(i);
+            if(c1>c2)
+            return s2;
+            else if(c2>c1)
+            return s1;
+        }
+        if(size==s1.length())
+        return s1;
+        else
+        return s2;
+    }
     int a,b;
     String ans="";
-    
-    public void update(String s){
-        if(ans.equals("")) {
-            ans=s;
-            return;
-        }
-        int size=Math.min(ans.length(),s.length());
-        for(int i=0;i<size;i++){
-            char c1=ans.charAt(i);
-            char c2=s.charAt(i);
-            if(c2<c1){
-                ans=s;
-                return;
-            }
-            else if(c1<c2)
-            return;
-        }
-        if(s.length()<ans.length())
-        ans=s;
-    }
-
     public void find(StringBuilder sb){
         String str=sb.toString();
         if(!seen.add(str))
         return;
-        update(str);
+        ans=check(ans,str);
 
         for(int i=1;i<sb.length();i=i+2){
             sb.setCharAt(i,(char)(((sb.charAt(i)-'0'+a)%10)+'0'));
@@ -49,7 +43,6 @@ class Solution {
         }
         find(rotated);
     }
-
     public String findLexSmallestString(String s,int a,int b){
         this.a=a;
         this.b=b%s.length();
