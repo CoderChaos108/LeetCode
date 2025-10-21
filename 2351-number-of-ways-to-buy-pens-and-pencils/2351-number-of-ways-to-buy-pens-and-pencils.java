5e125acf988b102ab382473f31eee43257fdfc3d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-21 23:06:17 +0530

    Time: 8 ms (99.57%), Space: 40.8 MB (34.2%) - LeetHub

--- 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.java
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.java
@@ -1,6 +1,10 @@
 class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long ans=0;
+        int a=Math.max(cost1,cost2);
+        int b=Math.min(cost1,cost2);
+        cost1=a;
+        cost2=b;
         for(int i=0;i*cost1<=total;i++){
             ans=ans+(total-cost1*i)/cost2+1;
         }
