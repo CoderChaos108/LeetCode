3c651359a4adf3425a50eb0c37e15daee4eb61d3
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-24 03:21:03 +0530

    Time: 2 ms (95.84%), Space: 52.5 MB (35.05%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        int m=score.length;
-        int n=score[0].length;
-        Arrays.sort(score,(a,b)->b[k]-a[k]);
-        return score;
+        Arrays.sort(score,(a,b)->b[k]-a[k]);return score;
     }
 }
