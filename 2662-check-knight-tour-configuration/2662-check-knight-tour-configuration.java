aacf2b471933b3c726853fa5a32c20a43831137d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-24 03:43:35 +0530

    Time: 1 ms (83.35%), Space: 44.6 MB (39.9%) - LeetHub

--- 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.java
@@ -2,19 +2,16 @@ class Solution {
     int n;
     int[][] grid;
     int[][] dir={{-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1}};
-    public boolean dfs(int i,int j,int move){
-        if(grid[i][j]==n*n-1){
-        if(move==n*n-1)
+    public boolean dfs(int i,int j){
+        if(grid[i][j]==n*n-1)
         return true;
-        return false;
-        }
         for(int[] a:dir){
             int ni=i+a[0];
             int nj=j+a[1];
-            if(ni<0||nj<0||ni>=n||nj>=n||grid[ni][nj]!=move+1)
+            if(ni<0||nj<0||ni>=n||nj>=n||grid[ni][nj]!=grid[i][j]+1)
             continue;
             else{
-                return dfs(ni,nj,move+1);
+                return dfs(ni,nj);
             }
         }
         return false;
@@ -22,6 +19,8 @@ class Solution {
     public boolean checkValidGrid(int[][] grid) {
         this.grid=grid;
         n=grid.length;
-        return dfs(0,0,0);
+        if(grid[0][0]!=0)
+        return false;
+        return dfs(0,0);
     }
 }
