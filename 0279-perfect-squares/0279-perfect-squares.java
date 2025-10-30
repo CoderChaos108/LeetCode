fb32902d20340dca1fe30ab0300b051480a790c0
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-10-31 01:44:03 +0530

    Time: 253 ms (7.94%), Space: 115.9 MB (5.26%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numSquares(int n) {
+        List<Integer> list=new ArrayList<>();
+        int num=1;
+        while(num*num<=n){
+            list.add(num*num);
+            num++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        HashSet<Integer> seen=new HashSet<>();
+        int steps=0;
+        for(int nums:list)
+        q.add(nums);
+        while(true){
+            steps++;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int s=q.poll();
+                if(!seen.add(s)||s>n)
+                continue;
+                if(s==n)
+                return steps;
+                for(int nums:list){
+                    q.add(s+nums);
+                }
+            }
+        }
+    }
+}
