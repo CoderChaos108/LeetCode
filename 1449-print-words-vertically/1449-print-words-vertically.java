68512697cccaf75239a43fef6949c40814ab564e
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-10 18:58:02 +0530

    Time: 1 ms (60.89%), Space: 43.1 MB (6.04%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> printVertically(String s) {
+        List<String> list=new ArrayList<>();
+        String[] words=s.split(" ");
+        int len=0;
+        for(String w:words)
+        len=Math.max(len,w.length());
+        for(int i=0;i<len;i++){
+            StringBuilder sb=new StringBuilder();
+            for(String w:words){
+                if(i>=w.length())
+                sb.append(" ");
+                else
+                sb.append(w.charAt(i));
+            }
+            int itr=sb.length()-1;
+            while(sb.charAt(itr)==' '){
+                itr--;
+                sb.setLength(sb.length()-1);
+            }
+            list.add(sb.toString());
+        }
+        return list;
+    }
+}
