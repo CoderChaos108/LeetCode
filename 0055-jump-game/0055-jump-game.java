b1ffa411a3616b50483ee622ec5847f4e2a1d042
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-15 21:41:29 +0530

    Time: 1249 ms (5%), Space: 47.5 MB (19.48%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean[] seen;
+    int[] nums;
+    boolean found=false;
+    int n;
+    public void jump(int k){
+        if(k>=seen.length)
+        return;
+        if(k==seen.length-1){
+        found=true;
+        return;
+        }
+        if(found||seen[k])
+        return;
+        seen[k]=true;
+        int lim=nums[k];
+        for(int i=1;i<=lim;i++){
+            if(k+i>=n)
+            return;
+            jump(k+i);
+        }
+    }
+    public boolean canJump(int[] nums) {
+        seen=new boolean[nums.length];
+        this.nums=nums;
+        n=nums.length;
+        jump(0);
+        return found;
+    }
+}
