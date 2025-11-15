fda580314bdcc3b90d0c5d89454b5f28d7410c2d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-15 22:30:26 +0530

    Time: 16 ms (18.64%), Space: 47.1 MB (17.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,25 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        Queue<Integer> q=new LinkedList<>();
-        q.add(0);
+        int goal=nums.length-1;
         int steps=0;
-        boolean[] seen=new boolean[nums.length];
-        while(true){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int idx=q.poll();
-                if(idx==nums.length-1)
-                return steps;
-                for(int j=1;j<=nums[idx];j++){
-                    if(j+idx>=nums.length)
-                    break;
-                    if(!seen[idx+j])
-                    q.add(idx+j);
-                    seen[idx+j]=true;
-                }
-
+        while(goal!=0){
+            int k=goal;
+            for(int i=goal-1;i>=0;i--){
+                if(nums[i]+i>=k)
+                goal=i;
             }
             steps++;
         }
+        return steps;
     }
 }
