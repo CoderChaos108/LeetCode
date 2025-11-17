35b85f22cb1fcb93e7ca3e8a9e880e4d11416a6c
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-17 19:57:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (27.49%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -10,7 +10,7 @@ class Solution {
             dp[i][j]=0;
             return 0;
         }
-        if(dp[i][j]!=0)
+        if(dp[i][j]!=-1)
         return dp[i][j];
         dp[i][j]=dfs(i,j+1)+dfs(i+1,j);
         return dp[i][j];
@@ -19,9 +19,14 @@ class Solution {
         a=obstacleGrid;
         m=a.length;
         n=a[0].length;
+        dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
         if(a[m-1][n-1]==1)
         return 0;
-        dp=new int[m][n];
         return dfs(0,0);
     }
 }
