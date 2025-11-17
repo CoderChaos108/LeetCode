891140178137646050a58beddce7ea2ba0b025b6
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-17 19:54:34 +0530

    Time: 4 ms (4.4%), Space: 43.4 MB (21.15%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[][] dp,a;
+    int m,n;
+    public int dfs(int i,int j){
+        if(i==m||j==n||(a[i][j]==1))
+        return 0;
+        if(i==m-1&&j==n-1)
+        return 1;
+        if(dp[i][j]!=0)
+        return dp[i][j];
+        dp[i][j]=dfs(i,j+1)+dfs(i+1,j);
+        return dp[i][j];
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        a=obstacleGrid;
+        m=a.length;
+        n=a[0].length;
+        if(a[m-1][n-1]==1)
+        return 0;
+        dp=new int[m][n];
+        return dfs(0,0);
+    }
+}
