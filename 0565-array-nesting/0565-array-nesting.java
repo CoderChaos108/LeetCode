8c7be96fcc6c630d7dd917800c631976985a409e
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-11-20 23:47:24 +0530

    Time: 5 ms (58.08%), Space: 70.5 MB (15.4%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -2,7 +2,8 @@ class Solution {
     public int arrayNesting(int[] nums) {
        boolean[] seen=new boolean[nums.length];
        int ans=0;
-        for(int i=0;i<nums.length;i++){
+       int len=nums.length;
+        for(int i=0;i<len;i++){
             int n=i;
             int c=0;
             while(!seen[n]){
