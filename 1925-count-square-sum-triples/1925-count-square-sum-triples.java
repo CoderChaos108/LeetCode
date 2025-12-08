06b96def6acdd5b25bbfc398cadef76cabc7e49d
Ayush Chauhan <ayushchauhanac0@gmail.com>
2025-12-08 20:38:19 +0530

    Time: 16 ms (37.37%), Space: 42.1 MB (37.58%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countTriples(int n) {
         int ans=0;
-        HashSet<Integer> squares=new HashSet<>();
-        for(int i=1;i<=n;i++)
-        squares.add(i*i);
         for(int i=1;i<=n;i++){
-            for(int j=i+1;Math.sqrt(i*i+j*j)<=n&&j<=n;j++){
-                if(squares.contains(i*i+j*j))
+            for(int j=i+1;j<=n;j++){
+                double c=Math.sqrt(i*i+j*j);
+                if(c>n)
+                break;
+                if(c%1==0.0)
                 ans++;
             }
         }
