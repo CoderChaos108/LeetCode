4040c9fd7a6fb4942803f74026fcecfbcb832485
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-01-08 02:28:54 +0530

    Time: 13 ms (23.64%), Space: 61 MB (12.02%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -1,19 +1,24 @@
 class Solution {
     int mod=1000000007;
-    HashMap<Integer,Long> dp=new HashMap<>();
+    int[] dp;
     public long count(int l,int low,int high,int zero,int one){
         if(l>high)
         return 0;
-        if(dp.containsKey(l))
-        return dp.get(l);
+        if(dp[l]!=-1)
+        return dp[l];
         int ret=0;
         if(l>=low&&l<=high){
             ret=1;
         }
-        dp.put(l,(ret+count(l+one,low,high,zero,one)+count(l+zero,low,high,zero,one))%mod);
-        return dp.get(l);
+        dp[l]=(int)((ret
+        + count(l+one,low,high,zero,one)
+        + count(l+zero,low,high,zero,one)) % mod);
+        return dp[l];
     }
     public int countGoodStrings(int low, int high, int zero, int one) {
+        dp=new int[high+1];
+        for(int i=0;i<dp.length;i++)
+        dp[i]=-1;
         return (int)count(0,low,high,zero,one);
     }
 }
