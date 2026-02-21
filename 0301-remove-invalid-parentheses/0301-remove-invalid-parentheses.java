4a4e1c08431016f3f322c5b7d4029711ce76632c
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-02-22 00:19:51 +0530

    Time: 69 ms (30.69%), Space: 53.3 MB (5.58%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -15,39 +15,34 @@ class Solution {
         }
         return st.isEmpty();
     }
-    HashSet<String> seen=new HashSet<>();
-    int found=-1;
-    List<String> list=new ArrayList<>();
-    public void backtrack(StringBuilder sb){
-        if(!seen.add(sb.toString()))
-        return;
-        if(check(sb.toString())){
-            list.add(sb.toString());
-            found=Math.max(found,sb.length());
-            return;
-        }
-        if(sb.length()<found)
-        return;
-        for(int i=0;i<sb.length();i++){
-            char c=sb.charAt(i);
-            if(c!='('&&c!=')')
-            continue;
-            sb.deleteCharAt(i);
-            backtrack(sb);
-            sb.insert(i,c);
-        }
-    }
     public List<String> removeInvalidParentheses(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        backtrack(sb);
-        int max=0;
-        for(String str:list)
-        max=Math.max(str.length(),max);
-        List<String> ans=new ArrayList<>();
-        for(String str:list){
-            if(str.length()==max)
-            ans.add(str);
+        StringBuilder builder=new StringBuilder(s);
+        List<String> set=new ArrayList<>();
+        Queue<StringBuilder> q=new LinkedList<>();
+        q.add(builder);
+        boolean found=false;
+        HashSet<String> seen=new HashSet<>();
+        while(!found){
+            int size=q.size();
+            while(size--!=0){
+                StringBuilder sb=q.poll();
+                if(!seen.add(sb.toString()))
+                continue;
+                if(check(sb.toString())){
+                    found=true;
+                    set.add(sb.toString());
+                    continue;
+                }
+                for(int i=0;i<sb.length();i++){
+                    char c=sb.charAt(i);
+                    if(c!='('&&c!=')')
+                    continue;
+                    sb.deleteCharAt(i);
+                    q.add(new StringBuilder(sb));
+                    sb.insert(i,c);
+                }
+            }
         }
-        return ans;
+        return new ArrayList<>(set);
     }
 }
