5373f429cbd241e3791665b2cc2cb90af9698c0b
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-03-12 22:25:03 +0530

    Time: 36 ms (4.4%), Space: 46.6 MB (5.49%) - LeetHub

--- 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.java
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.java
@@ -1,34 +1,38 @@
 class Solution {
-    int[][][][] dp;
-    public int find(int l,String last,int x,int y,int z){
-        int state=-1;
-        if(last.equals("AA"))
-        state=0;
-        else if(last.equals("BB"))
-        state=1;
-        else if(last.equals("AB"))
-        state=2;
-        if(state!=-1){
-            if(dp[x][y][z][state]!=0)
-            return dp[x][y][z][state];
+
+    int[][][] dp;
+
+    int dfs(int x,int y,int state){
+        if(x==0 && y==0)
+        return 0;
+
+        if(dp[x][y][state]!=0)
+            return dp[x][y][state];
+
+        int ans=0;
+
+        
+        if(x>0 && state!=0){
+            ans=Math.max(ans,2+dfs(x-1,y,0));
         }
-        int ans=l;
-        if(!last.equals("AA")){
-            if(x>0)
-            ans=find(2+l,"AA",x-1,y,z);
-            if(z>0){
-                ans=Math.max(ans,find(2+l,"AB",x,y,z-1));
-            }
+
+        
+        if(y>0 && state!=1){
+            ans=Math.max(ans,2+dfs(x,y-1,1));
         }
-        if(!last.equals("AB")&&!last.equals("BB")&&y>0){
-            ans=Math.max(ans,find(l+2,"BB",x,y-1,z));
-        }
-        if(state!=-1)
-        dp[x][y][z][state]=ans;
-        return ans;
+
+        return dp[x][y][state]=ans;
     }
-    public int longestString(int x, int y, int z) {
-        dp=new int[x+1][y+1][z+1][3];
-        return find(0,"",x,y,z);
+
+    public int longestString(int x,int y,int z) {
+
+        int base=2*z;
+
+        dp=new int[x+1][y+1][3];
+        
+
+        int best=dfs(x,y,2);
+
+        return base+best;
     }
 }
