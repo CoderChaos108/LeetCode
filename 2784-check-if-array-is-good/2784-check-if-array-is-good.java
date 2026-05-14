0a520878cd076ebe114ac4090d198a86361e2ed4
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-05-14 22:03:49 +0530

    Time: 2 ms (59.62%), Space: 44.9 MB (52%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isGood(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
         int n=nums.length-1;
-        Arrays.sort(nums);
-        for(int i=1;i<=n;i++){
-            if(nums[i-1]!=i)
+        for(int i:nums)
+        hm.put(i,hm.getOrDefault(i,0)+1);
+        for(int i=1;i<n;i++){
+            int f=hm.getOrDefault(i,0);
+            if(f!=1)
             return false;
         }
-        return nums[n]==n;
+        int f=hm.getOrDefault(n,0);
+        return f==2;
     }
 }
