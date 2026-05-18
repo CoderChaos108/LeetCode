5eb10c38b337ac8d91167ca3aaa4e170e43e7c9e
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-05-19 00:19:58 +0530

    Time: 99 ms (5.57%), Space: 140.9 MB (5.17%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    int n;
+    HashMap<Integer,HashSet<Integer>> hm;
+    public int minJumps(int[] arr) {
+        n=arr.length;
+         hm=new HashMap<>();
+        int idx=0;
+        for(int i:arr){
+            if(!hm.containsKey(i))
+            hm.put(i,new HashSet<>());
+            hm.get(i).add(idx++);
+        }
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] seen=new boolean[n];
+        int steps=0;
+        q.add(0);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int id=0;id<size;id++){
+                int i=q.poll();
+                if(i==n-1)
+                return steps;
+                int val=arr[i];
+                int[] moves={i-1,i+1};
+                HashSet<Integer> list=hm.get(val);
+                if(seen[i])
+                continue;
+                seen[i]=true;
+                for(int ni:moves){
+                    if(ni<0||ni>=n)
+                    continue;
+                    q.add(ni);
+                }
+                for(int ni:list){
+                    if(ni<0||ni>=n)
+                    continue;
+                    q.add(ni);
+                }
+                hm.put(val,new HashSet<>());
+            }
+            steps++;
+        }
+        return -1;
+    }
+}
