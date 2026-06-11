b47fc0b14cd2acb79fd2d05670fe3c582b6be66b
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-06-12 04:04:03 +0530

    Time: 143 ms (52.17%), Space: 294.3 MB (56.52%) - LeetHub

---
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.java
@@ -0,0 +1,95 @@
+class Solution {
+    int n;
+    int mod=1000000007;
+    List<Integer>[] graph;
+    int[] parent;
+    int[][] up;
+    int[] level;
+    int d=0;
+    public int pow(long a,long b){
+        long res=1;
+        while(b>0){
+            if((b&1)!=0)
+            res=((res*a)%mod);
+            a=(a*a)%mod;
+            b=b>>1;
+        }
+        return (int)(res%mod);
+    }
+    public void builder(int node,int par,int l){
+        parent[node]=par;
+        level[node]=l;
+        d=Math.max(d,l);
+        for(int neigh:graph[node]){
+            if(neigh==par)
+            continue;
+            builder(neigh,node,l+1);
+        }
+    }
+    public int kth(int node,int k){
+        if(node==-1||k==0)
+        return node;
+        int log=(int)Math.floor(Math.log(k)/Math.log(2));
+        int red=(int)(Math.pow(2,log));
+        return kth(up[node][log],k-red);
+    }
+    public int lca(int u,int v){
+        if(level[u]<level[v]){
+            int temp=u;
+            u=v;
+            v=temp;
+        }
+        int k=level[u]-level[v];
+        u=kth(u,k);
+        if(u==v)
+        return u;
+        int log=(int)Math.floor(Math.log(d)/Math.log(2));
+        for(int i=log;i>=0;i--){
+            if(up[u][i]!=up[v][i]){
+                u=up[u][i];
+                v=up[v][i];
+            }
+        }
+        return Math.max(1,parent[u]);
+    }
+    public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
+        n=edges.length+1;
+        level=new int[n+1];
+        graph=new ArrayList[n+1];
+        for(int i=1;i<=n;i++)
+        graph[i]=new ArrayList<>();
+        for(int[] arr:edges){
+            int u=arr[0];
+            int v=arr[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        parent=new int[n+1];
+        builder(1,-1,0);
+        int log=(int)Math.floor(Math.log(n)/Math.log(2));
+        up=new int[n+1][log+1];
+        for(int i=1;i<=n;i++)
+        up[i][0]=parent[i];
+        for(int j=1;j<=log;j++){
+            for(int i=1;i<=n;i++){
+                if(up[i][j-1]!=-1)
+                up[i][j]=up[up[i][j-1]][j-1];
+                else
+                up[i][j]=-1;
+            }
+        }
+        int q=queries.length;
+        int[] a=new int[q];
+        for(int i=0;i<q;i++){
+            int u=queries[i][0];
+            int v=queries[i][1];
+            int l=lca(u,v);
+            int x=level[u]+level[v]-2*level[l];
+            int ans=pow(2,x-1);
+            if(u==v)
+            ans=0;
+            a[i]=(int)ans;
+        }
+        return a;
+    }
+}
