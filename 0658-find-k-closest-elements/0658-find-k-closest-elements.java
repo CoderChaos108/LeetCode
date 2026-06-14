5e1a0c8741ee014b1c90ea549d01c09e285117a8
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-06-15 02:17:09 +0530

    Time: 5 ms (57.11%), Space: 47.3 MB (98.05%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,57 +1,65 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int l=0;
-        int r=arr.length-1;
-        int p=-1;
-        int q=arr.length;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(arr[m]<=x){
-                l=m+1;
-                p=m;
+        int l = 0;
+        int r = arr.length - 1;
+
+        int p = -1;
+        int q = arr.length;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (arr[m] <= x) {
+                p = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
             }
-            else
-            r=m-1;
         }
-        l=0;
-        r=arr.length-1;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(arr[m]>x){
-                q=m;
-                r=m-1;
+
+        l = 0;
+        r = arr.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (arr[m] > x) {
+                q = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
             }
-            else
-            l=m+1;
         }
-        int idx=0;
-        int[] a=new int[k];
-        while(idx!=k){
-            if(p<0){
-                a[idx++]=arr[q++];
+
+        int count = 0;
+
+        while (count < k) {
+            if (p < 0) {
+                q++;
             }
-            else if(q>=arr.length){
-                a[idx++]=arr[p--];
+            else if (q >= arr.length) {
+                p--;
             }
-            else{
-                int le=arr[p];
-                int re=arr[q];
-                if(Math.abs(le-x)<Math.abs(re-x)){
-                    a[idx++]=arr[p--];
+            else {
+                int leftDist = Math.abs(arr[p] - x);
+                int rightDist = Math.abs(arr[q] - x);
+
+                if (leftDist <= rightDist) {
+                    p--;
+                } else {
+                    q++;
                 }
-                else if(Math.abs(le-x)>Math.abs(re-x))
-                a[idx++]=arr[q++];
-                else{
-                    if(le<re)
-                    a[idx++]=arr[p--];
-                    else
-                    a[idx++]=arr[q++];
-                }
             }
+
+            count++;
         }
-        List<Integer> list=new ArrayList<>();
-        for(int i=p+1;i<q;i++)
-        list.add(arr[i]);
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = p + 1; i < q; i++) {
+            list.add(arr[i]);
+        }
+
         return list;
     }
 }
