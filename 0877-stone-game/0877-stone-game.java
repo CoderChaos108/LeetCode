3065a01644d20d5a601b52038699ed4713da069a
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-06-24 22:53:08 +0530

    Time: 9 ms (16.66%), Space: 47.3 MB (9.11%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,10 +1,12 @@
 class Solution {
     int[][] dp;
+    boolean[][] seen;
     public int find(int l,int r,int[] a){
         if(l>r)
         return 0;
-        if(dp[l][r]!=-1)
+        if(seen[l][r])
         return dp[l][r];
+        seen[l][r]=true;
         int left=a[l]-find(l+1,r,a);
         int right=a[r]-find(l,r-1,a);
         return dp[l][r]=Math.max(left,right);
@@ -12,8 +14,7 @@ class Solution {
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
         dp=new int[n][n];
-        for(int i=0;i<n;i++)
-        Arrays.fill(dp[i],-1);
+        seen=new boolean[n][n];
         return find(0,n-1,piles)>0;
     }
 }
