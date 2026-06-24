c4e06c77c16f9c478d949cca0b33ed7548aa69c6
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-06-24 21:32:23 +0530

    Time: 3 ms (14.98%), Space: 42.3 MB (95.15%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] dp;
+    public int find(int k,int[] a){
+        if(k==0)
+        return 1;
+        if(dp[k]!=-1)
+        return dp[k];
+        int res=0;
+        for(int i:a){
+            if(k-i<0)
+            break;
+            res=res+find(k-i,a);
+        }
+        dp[k]=res;
+        return res;
+    }
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        dp=new int[target+1];
+        Arrays.fill(dp,-1);
+        return find(target,nums);
+    }
+}
