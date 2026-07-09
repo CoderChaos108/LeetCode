0f0d9213f8a5cafcee23c79c7b8e47b74eb6db1c
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-07-09 12:38:35 +0530

    Time: 55 ms (5.94%), Space: 163 MB (68.32%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[] parent;
+    public int findParent(int node){
+        if(parent[node]==node)
+        return node;
+        return parent[node]=findParent(parent[node]);
+    }
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        parent=new int[n];
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+        for(int i=0;i<n;i++){
+            int lim=nums[i]+maxDiff;
+            int l=i;
+            int r=n-1;
+            int res=i;
+            while(l<=r){
+                int m=(l+r)/2;
+                if(nums[m]<=lim){
+                    l=m+1;
+                    res=m;
+                }
+                else
+                r=m-1;
+            }
+            int j=res;
+            while(findParent(j)!=findParent(i)){
+                parent[j]=findParent(i);
+                j--;
+            }
+        }
+        boolean[] ans=new boolean[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int u=queries[i][0];
+            int v=queries[i][1];
+            if(findParent(u)==findParent(v))
+            ans[i]=true;
+        }
+        return ans;
+    }
+}
