d39dca70135fa11ef6da202e6ad5c5f903d9502f
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-07-22 23:21:56 +0530

    Time: 295 ms (31.89%), Space: 47.3 MB (6.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,26 @@
 class Solution {
-    int ans=0;
-    int target;
-    public void find(int[] a,int sum,int k){
-        if(k==a.length){
+    HashMap<List<Integer>,Integer> hm=new HashMap<>();
+    int n;
+    int s;
+    public int find(int i,int sum,int[] a,int target){
+        List<Integer> list=Arrays.asList(i,sum);
+        if(i>=n){
             if(sum==target)
-            ans++;
-            return;
+            return 1;
+            return 0;
         }
-        find(a,sum+a[k],k+1);
-        find(a,sum-a[k],k+1);
+        int res=0;
+        if(hm.containsKey(list))
+        return hm.get(list);
+        res=find(i+1,sum,a,target)+find(i+1,sum-2*a[i],a,target);
+        hm.put(list,res);
+        return res;
     }
-    public int findTargetSumWays(int[] nums, int target2) {
-        target=target2;
-        find(nums,0,0);
-        return ans;
+    public int findTargetSumWays(int[] nums, int target) {
+        n=nums.length;
+        s=0;
+        for(int i:nums)
+        s=s+i;
+        return find(0,s,nums,target);
     }
 }
