5e83c8f29f78a25966e18d08560c98fbc2a90b9c
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-07-28 23:59:24 +0530

    Time: 10 ms (24.19%), Space: 49.5 MB (5.93%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[] dp;
+    int n;
+    public int find(int i,int[] a){
+        if(i==n)
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
+        int skip=find(i+1,a);
+        int take=0;
+        int j=i;
+        while(j<n&&a[j]==a[i])
+        take=take+a[j++];
+        while(j<n&&a[j]==a[i]+1)
+        j++;
+        return dp[i]=Math.max(skip,take+find(j,a));
+    }
+    public int deleteAndEarn(int[] nums) {
+        Arrays.sort(nums);
+        n=nums.length;
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+        return find(0,nums);
+    }
+}
