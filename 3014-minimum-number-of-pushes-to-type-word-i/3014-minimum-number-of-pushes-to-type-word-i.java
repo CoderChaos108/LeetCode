b8a6a0b2f17d7d3ec12d41cb4d3109969021b890
Ayush Chauhan <ayushchauhanac0@gmail.com>
2026-07-30 20:28:53 +0530

    Time: 0 ms (100%), Space: 43 MB (79.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,19 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> hm=new HashMap<>();
-        int ans=0;
-        for(int i=0;i<word.length();i++){
-            char c=word.charAt(i);
-            hm.put(c,hm.getOrDefault(c,0)+1);
-        }
-        List<Integer> list=new ArrayList<>();
-        for(char key:hm.keySet()){
-            list.add(hm.get(key));
-        }
-       
-        for(int j=0;j<list.size();j++){
+        
+       int ans=0;
+        for(int j=0;j<word.length();j++){
             int push=j/8+1;
-            ans=ans+push*list.get(j);
+            ans=ans+push;
 
         }    
         return ans;
